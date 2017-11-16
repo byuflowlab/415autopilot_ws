d324d5f2a3d8f4ec1cd3be3f06a84e9527000db0
Andrew Ning <aning@byu.edu>
2017-11-16 14:05:43 -0700

    cleanup.  added boundary checks. more accurate params

--- src/autopilot_me415/autopilot.py
+++ src/autopilot_me415/autopilot.py
@@ -5,27 +5,43 @@ import numpy as np
 
 
 class autopilot:
+
     def __init__(self):
+        # GPS update rate (also command update rate)
         self.update_rate = 10  # Hz
 
+        # defile floor/ceiling
+        self.elevation = 1508.0
+        ft2m = 0.3048
+        alt_tolerance = 25*ft2m
+        self.floor = self.elevation + 100*ft2m
+        self.ceiling = self.elevation + 400*ft2m
+        self.floorwarn = self.floor + alt_tolerance
+        self.ceilingwarn = self.ceiling - alt_tolerance
+
+        # define boundary
+        self.bdy_center = [40.2672305, -111.635524]
+        self.bdy_R = [98.353826748828595, 127.49653449395105]
+        self.bdy_theta = -15*np.pi/180.0
+
+        # define waypoints
+        self.wp1 = [40.26702297859316, -111.63508476781465]
+        self.wp2 = [40.267641059243935, -111.63587333726502]
+        self.wp3 = [40.26658909078882, -111.6358518795929]
+        self.wp4 = [40.26778432216462, -111.63506331014253]
+        self.wp_state_machine = 0
+        self.wp_tolerance = 10.0  # m
+
+        # subscripte to GPS
         self.gps_subscriber = rospy.Subscriber("/fixedwing/gps", GPS, self.gpsCallback, queue_size=10)
         self.currentGPS = GPS()
 
+        # set timer and command publisher
         rospy.Timer(rospy.Duration(1./self.update_rate), self.control)
         rospy.Timer(rospy.Duration(1./self.update_rate), self.check_status)
         self.command_publisher = rospy.Publisher("/fixedwing/command", Command, queue_size=1)
 
-        self.wp_state_machine = 0
-        self.minAlt = rospy.get_param('~minAlt', 1387.0 + 30.48)  # default is 100 ft
-        self.maxAlt = rospy.get_param('~maxAlt', 1387.0 + 121.92)  # default is 400 ft
-        self.wp1_lat = rospy.get_param('~wp1_lat', 40.267110)
-        self.wp1_lon = rospy.get_param('~wp1_lon', -111.634983)
-        self.wp2_lat = rospy.get_param('~wp2_lat', 40.267492)
-        self.wp2_lon = rospy.get_param('~wp2_lon', -111.635755)
-        self.wp3_lat = rospy.get_param('~wp3_lat', 40.266725)
-        self.wp3_lon = rospy.get_param('~wp3_lon', -111.635809)
-        self.wp4_lat = rospy.get_param('~wp4_lat', 40.267993)
-        self.wp4_lon = rospy.get_param('~wp4_lon', -111.634930)
+
 
     def gpsCallback(self, msg):
         if msg.fix and msg.NumSat > 3:
@@ -55,80 +71,73 @@ class autopilot:
 
     def check_status(self, event):
 
-        if self.currentGPS.altitude < self.minAlt:
-            print "altitude too low!"
+        # ---- altitude checks -----
+        alt = self.currentGPS.altitude
+
+        if alt < self.floor:
+            print "ALERT!  Altitude too low! Resume manual control!"
+            return
+        elif alt < self.floorwarn:
+            print "WARNING!  Altitude approaching floor! Ready pilot."
+            return
+        elif alt > self.ceiling:
+            print "ALERT!  Altitude too high! Resume manual control!"
+            return
+        elif alt > self.ceilingwarn:
+            print "WARNING!  Altitude approaching ceiling! Ready pilot."
             return
 
-        if self.currentGPS.altitude > self.maxAlt:
-            print "altitude too high!"
+
+        # ---- boundary checks ----
+        pos = [self.currentGPS.latitude, self.currentGPS.longitude]
+
+        ell = self.ellipse(pos)
+        if ell > 1.0:
+            print "ALERT!  Out of bounds! Resume manual control!"
+            return
+        elif ell > 0.7:
+            print "WARNING!  Close to boundary! Ready pilot."
             return
 
-        if self.wp_state_machine == 0 and self.distance(self.wp1_lat, self.wp1_lon) < 10:
+
+        # ---- waypoint checks -----
+
+        if self.wp_state_machine == 0 and self.distance(pos, self.wp1) < self.wp_tolerance:
             self.wp_state_machine = 1
-            print "achieved waypoint 1!"
-
-        elif self.wp_state_machine == 1 and self.distance(self.wp2_lat, self.wp2_lon) < 10:
+            print "YES!  Achieved waypoint 1!"
+        elif self.wp_state_machine == 1 and self.distance(pos, self.wp2) < self.wp_tolerance:
             self.wp_state_machine = 2
-            print "achieved waypoint 2!"
-
-        elif self.wp_state_machine == 2 and self.distance(self.wp3_lat, self.wp3_lon) < 10:
+            print "YES!  Achieved waypoint 2!"
+        elif self.wp_state_machine == 2 and self.distance(pos, self.wp3) < self.wp_tolerance:
             self.wp_state_machine = 3
-            print "achieved waypoint 3!"
-
-        elif self.wp_state_machine == 3 and self.distance(self.wp4_lat, self.wp4_lon) < 10:
+            print "YES!  Achieved waypoint 3!"
+        elif self.wp_state_machine == 3 and self.distance(pos, self.wp4) < self.wp_tolerance:
             self.wp_state_machine = 4
-            print "achieved waypoint 4!"
-            print "all waypoints achieved !!!"
+            print "YES!  Achieved waypoint 4!"
+            print "CONGRATULATIONS! All waypoints achieved!!!"
 
-    # def check_boundary(self, event):
-    #     #---Define Boundary---#
-    #     #majoraxis end points of boundary ellipse (Google Maps)
-    #     ellipsemajor = np.array([40.268316 -111.635040; 40.266145 -111.636008])
-    #
-    #     #Midpoint of Major Axis set as center of ellipse
-    #     center = np.array([mean(ellispemajor[:,0]), ellimpsmajor(wps[:,1])]) #center point of waypoints
-    #
-    #     #Selected point for Minor Axis Radius (Google Maps)
-    #     ellipseminor = np.array([40.266994 -111.634407]) #rotated longitude axis point around the orange line
-    #
-    #     #Radius values for Major and Minor Axes
-    #     rminalarm = np.sqrt((ellipsemionor[0]-center[0])**2 + (ellipseminor[1]-center[1])**2) #rotated longitude radius
-    #     rmajalarm = np.sqrt((ellipsejamor[0,0]-center[0])**2 + (ellipsejamor[1,0]-center[1])**2) #rotated loatitude radius
-    #
-    #     #Selectd Rotation Angle
-    #     rotate = -15*np.pi #angle of axes rotation 75 degrees from eastward axis.
-    #
-    #     #---Equation of rotated Ellipse---#
-    #     def ellipse(lon,lat,clon,clat,theta,rmin,rmaj):
-    #         #lon = gps longitude
-    #         #lat = gps latitude
-    #         #clon = longitude center (center[0])
-    #         #clat = latitude center (center[1])
-    #         #theta = angle of axes rotation
-    #         #rmin = minor axis radius (rotated lonitude axis)
-    #         #rmaj = major axis radius (rotated latitude axis)
-    #
-    #         return (((lon-clon)*np.cos(theta)+(lat-clat)*np.sin(theta))**2)/(rmin**2) + (((lon-clon)*np.sin(theta)-(lat-clat)*np.cos(theta))**2)/(rmaj**2)-1
-    #
-    #     #---Check Current Position---#
-    #     #Alarm value (1 = on ellipse)
-    #     alarm = ellipse(self.currentGPS.lattitude,self.currentGPS.longitude,center[1],center[0],rotate,rminalarm,rmajalarm)
-    #
-    #     #---Print Warnings/Alarms if Necessary---#
-    #     #Print Warning when approaching boundary
-    #     if alarm > 0.9 && alarm <= 1.0:
-    #         rospy.logwarn("WARNING: APPROACHING NO FLY BOUNDARY.")
-    #
-    #     #Print Alarm when crossing boundary
-    #     if alarm > 1.0:
-    #         rospy.logerr("ALERT!: NO FLY BOUNDARY BREACHED. RESUME MANUAL CONTROL.")
 
-    def distance(self, wp_lat, wp_lon):
+    def distance(pt1, pt2):
+        """pt = [lat, long]"""
         EARTH_RADIUS = 6371000.0
-        distN = EARTH_RADIUS*(self.currentGPS.latitude - wp_lat)*np.pi/180.0
-        distE = EARTH_RADIUS*np.cos(wp_lat*np.pi/180.0)*(self.currentGPS.longitude - wp_lon)*np.pi/180.0
-        return np.linalg.norm(np.array([distN, distE]))
+        distN = EARTH_RADIUS*(pt2[0] - pt1[0])*np.pi/180.0
+        distE = EARTH_RADIUS*np.cos(pt1[0]*np.pi/180.0)*(pt2[1] - pt1[1])*np.pi/180.0
+        return distE, distN, np.linalg.norm([distN, distE])
 
+
+    def ellipse(pt):
+        """check if point is outside of boundary ellipse (ell > 1)"""
+
+        center = self.bdy_center
+        R = self.bdy_R
+        theta = self.bdy_theta
+
+        dx, dy, _ = distance(center, pt)
+        ct = np.cos(theta)
+        st = np.sin(theta)
+        ell = ((dx*ct + dy*st)/R[0])**2 + ((dx*st - dy*ct)/R[1])**2
+
+        return ell
 
 if __name__ == '__main__':
     rospy.init_node('autopilot_py', anonymous=True)
